d2a5fed369aa36189deece85460975efc61cde30
starpech <im_pech@hotmail.com>
2020-09-14 21:29:10 +0700

    Web Pech

---
+++ Pech/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MY WEB PAGE</title>
+</head>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+</style>
+
+<body>
+    <p><h1 style="background-color:red">Hollo This is my webpage</h1></p>
+    <h3>Hry</h3>
+</body>
+</html>
