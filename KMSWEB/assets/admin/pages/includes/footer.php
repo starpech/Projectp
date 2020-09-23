c2f95aeb6c98c7a0ecb30f7f8643b6c091b24b2e
starpech <im_pech@hotmail.com>
2020-09-23 12:08:59 +0700

    ลบ KMSWEB

--- KMSWEB/assets/admin/pages/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0
-    </div>
-    <strong>Copyright &copy; 2020 </strong>KMS Dev. All right reserve
-</footer>
--- KMSWEB/phpjasperxml-master/sample/setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server="localhost";
-$db="phpjasperxml";
-$user="root";
-$pass="";
-$version="1.1";
-
-$pgport=5432; //only for postgresql
