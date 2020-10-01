0c7ee047b9dcc398b68053f6d915a969fb738079
starpech <im_pech@hotmail.com>
2020-10-01 20:05:58 +0700

    s
    3e

--- Learn PHP/connect_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn = mysql_connect('localhost','root','');
-    if($conn){
-        $db_con = mysql_select_db ('guestbook_db');
-        mysql_quely("SET NAMES utf8");
-    } else {
-        echo "connect fail !";
-    }
-?>
